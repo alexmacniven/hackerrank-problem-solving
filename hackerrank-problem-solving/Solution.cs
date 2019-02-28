fa3dff4fb93d76b8af2b7d238587b04ed93d6f7b
Alex Macniven <apmacniven@outlook.com>
2019-02-28 17:30:03 +0000

    Adds CompareTheTriplets solution

---
+++ hackerrank-problem-solving/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompareTheTriplets
+{
+    class Solution
+    {
+        /*
+           CompareTriplets solves the problem outlined at
+           https://www.hackerrank.com/challenges/compare-the-triplets/problem
+        */
+
+        static List<int> CompareTriplets(List<int> a, List<int> b)
+        {
+            int a_win = 0;
+            int b_win = 0;
+            List<int> results = new List<int>();
+
+            for (int i = 0; i < a.Count - 1; i++)
+            {
+                if (a[i] > b[i])
+                {
+                    a_win++;
+                }
+                else if (a[i] < b[i])
+                {
+                    b_win++;
+                }
+            }
+            results.Add(a_win);
+            results.Add(b_win);
+            return results;
+
+        }
+    }
+}
